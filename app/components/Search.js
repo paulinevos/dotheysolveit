3e94a970acf7e1036f7f2027a2b000c7bd23e2f7
Pauline Vos <pvos88@gmail.com>
2021-03-26 21:42:14 +0100

    Style fonts and search form

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,4 +1,32 @@
 import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const Form = styled.form`
+  display: flex;
+  margin-top:50px;
+  align-self:center;
+`
+const Input = styled.input`
+  padding: 13px 20px;
+  font-family: 'Open Sans', sans-serif;
+  border: 1px solid #CCCCCC;
+  margin-right:10px;
+`
+
+const Button = styled.button`
+  padding: 13px 30px;
+  font-family: 'Open Sans', sans-serif;
+  font-size:0.8em;
+  background-color:#a3222a;
+  color: #FFFFFF;
+  border: none;
+  :active {
+    background-color:#991509;
+  }
+  :disabled {
+    opacity:0.6;
+  }
+`
 
 export default function Search()
 {
@@ -10,24 +38,25 @@ export default function Search()
   }
 
   return(
-    <form
+    <Form
       name='title-search'
       id='search'
       onSubmit={handleSubmit}
     >
-      <input
+      <Input
         type='text'
         id='title'
         placeholder='Find a title'
         value={value}
         onChange={e=>setValue(e.target.value)}
       />
-      <button
+      <Button
         className='btn'
         type='submit'
+        disabled={!value}
       >
-        Do they solve it?
-      </button>
-    </form>
+        TELL ME.
+      </Button>
+    </Form>
   )
 }
--- app/index.js
+++ app/index.js
@@ -2,16 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Search from './components/Search'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-top: 10%;
+  width:900px;
+`
 
 class App extends React.Component {
   render() {
     return(
-      <>
+      <Container>
         <h1>Do They Solve It?</h1>
         <p>If you like true crime, but you don't like wasting your life watching cases they never solve,
           find out here if they solve it at the end.</p>
         <Search />
-      </>
+      </Container>
     )
   }
 }
