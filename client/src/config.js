5b5e42dd351f60ef4311a1d4c6c2d44164891397
Pauline Vos <pauline.vos@werkspot.nl>
2021-11-01 13:52:18 +0100

    Source env vars for API key

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   movieDatabase: {
-    apiKey: '',
+    apiKey: process.env.REACT_APP_API_KEY,
     baseUrl: 'https://api.themoviedb.org/3/',
     genreIds: [80, 99, 9648],
     mediaTypes: ['movie', 'tv']
